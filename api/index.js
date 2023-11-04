32157f9ff1ab4e04c5e990de04812697b6b34899
Preston Roesslet <proesslet@gmail.com>
2023-11-04 14:11:31 -0500

    added comments

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Allow cross-origin requests from http://localhost:5173
 var cors = require("cors");
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -10,23 +11,33 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+
+// Parse incoming request bodies as JSON. This will be
+// what allows us to access the data in the request body
 app.use(bodyParser.json());
 
+// GET request endpoint to retrieve my name
 app.get("/myname", (req, res) => {
   res.send("Preston Roesslet");
 });
 
+// Since we do not have a database for this workshop, we will
+// use a simple array to store a list of people in memory.
+// This array will be reset every time the server restarts.
 var people = [];
 
+// Post request endpoint to add a person to the people array
 app.post("/addPerson", (req, res) => {
   people.push(req.body.name);
   res.status(200).json({ message: "Person added successfully" });
 });
 
+// GET request endpoint to retrieve all people in the people array
 app.get("/getPeople", (req, res) => {
   res.status(200).json({ people: people });
 });
 
+// Start app listening on port 3000
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
--- hoot-workshop5/src/components/UserForm.vue
+++ hoot-workshop5/src/components/UserForm.vue
@@ -1,6 +1,7 @@
 <template>
   <form>
     <input v-model="name" type="text" placeholder="Name" />
+    <!--Using @click, this button will run the submit method when clicked-->
     <button type="button" @click="submit">Submit</button>
   </form>
   <p>{{ message }}</p>
@@ -18,10 +19,13 @@ export default {
     };
   },
   methods: {
+    // This method will send a POST request to the specified URL
+    // and send the name in the form as the data
     submit() {
       axios("http://localhost:3000/addPerson", {
         method: "POST",
         data: {
+          // "this" refers to the Vue instance and allows us to access the data above
           name: this.name,
         },
       })
@@ -31,6 +35,9 @@ export default {
         .catch((error) => {
           this.message = error;
         });
+
+      // The above method is asynchronous, so we must use "then" and "catch" to handle the response
+      // from the server.
     },
   },
 };
